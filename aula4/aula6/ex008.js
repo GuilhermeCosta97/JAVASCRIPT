f5ee30dab50d0285ab201cfdd3b95f52971eae55
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2024-06-15 17:00:33 -0300

    novos exercicios

---
+++ aula4/aula6/ex008.js
@@ -0,0 +1,7 @@
+var vel = 30
+
+console.log(`seu carro esta a ${vel}km/hr`)
+if (vel > 60) { // condição simples
+    console.log(`Você passou da velocidade permitida, voçê foi Multado!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
---
+++ aula4/aula6/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Você esta vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Voçê é brasileiro!')
+} else {
+    console.log('Voçê é estrangeiro!')
+}
