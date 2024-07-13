aee1c864171f4b8f0f00eb721bda7aa3f43e168b
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2024-07-13 20:46:42 -0300

    +ex

---
+++ contador/script.js
@@ -0,0 +1,34 @@
+function contar() {
+    // Lógica da função aqui
+    console.log("Button clicked!");
+    // Substitua console.log pela sua funcionalidade real
+
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.value.length == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3c1}`
+}
+} 
