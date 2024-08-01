a03ba970f485bcd57003f39770e8cd4b96d8827c
GuilhermeCosta97 <167794185+GuilhermeCosta97@users.noreply.github.com>
2024-08-01 11:00:01 -0300

    ex fin

--- analisador/script.js
+++ analisador/script.js
@@ -12,7 +12,7 @@ function isNumero(n) {
 }
 
 function inlista(n, l) {
-    if (l.indexOf(Number(n)) != -1) { // Corrigido para indexOf
+    if (l.indexOf(Number(n)) != -1) { 
         return true
     } else {
         return false
@@ -28,5 +28,37 @@ function adicionar() {
         res.innerHTML = '' 
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar');
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        for (let pos in valores) {
+            soma += valores[pos];
+            if (valores[pos] > maior)
+                maior = valores[pos];
+            if (valores[pos] < menor)
+                menor = valores[pos];
+        }
+
+        media = soma / tot;
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`;
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`;
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`;
     }
 }
